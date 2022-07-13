78c68d3a54ff1a5b8763a4cfd3857c2f23e900eb
Sue Young <m.sue.young@gmail.com>
2022-07-12 18:37:26 -0600

    Enhancing the style for readability

--- js/app.js
+++ js/app.js
@@ -8,59 +8,59 @@ alert(`Welcome to my site ${siteVisitor}! Guess these facts about me.`);
 let questionOne = prompt('Do I have children? (yes/no or y/n)').toLowerCase();
 if(questionOne === 'y' || questionOne === 'yes'){
   alert('You are correct!');
-  console.log('You are correct!');
+//   console.log('You are correct!');
 } else if(questionOne === 'n' || questionOne === 'no'){
   alert('You are wrong.');
-  console.log('You are wrong.');
+//   console.log('You are wrong.');
 } else{
   alert('Answer yes/no or y/n');
-  console.log('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
 }
 
 let questionTwo = prompt('Do I have one child? (yes/no or y/n)').toLowerCase();
 if(questionTwo === 'y' || questionTwo === 'yes'){
   alert('You are correct!');
-  console.log('You are correct!');
+//   console.log('You are correct!');
 } else if(questionTwo === 'n' || questionTwo === 'no'){
   alert('You are wrong.');
-  console.log('You are wrong.');
+//   console.log('You are wrong.');
 } else{
   alert('Answer yes/no or y/n');
-  console.log('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
 }
 
 let question = prompt('Is it a boy? (yes/no or y/n)').toLowerCase();
 if(question === 'y' || question === 'yes'){
   alert('You are correct!');
-  console.log('You are correct!');
+//   console.log('You are correct!');
 } else if(question === 'n' || question === 'no'){
   alert('You are wrong.');
-  console.log('You are wrong.');
+//   console.log('You are wrong.');
 } else{
   alert('Answer yes/no or y/n');
-  console.log('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
 }
 
 question = prompt('Is his favorite food asian? (yes/no or y/n)').toLowerCase();
 if(question === 'y' || question === 'yes'){
   alert('You are correct!');
-  console.log('You are correct!');
+//   console.log('You are correct!');
 } else if(question === 'n' || question === 'no'){
   alert('You are wrong.');
-  console.log('You are wrong.');
+//   console.log('You are wrong.');
 } else{
   alert('Answer yes/no or y/n');
-  console.log('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
 }
 
 question = prompt('Does he like to play video games? (yes/no or y/n)').toLowerCase();
 if(question === 'y' || question === 'yes'){
   alert(`You are correct ${siteVisitor}!`);
-  console.log(`You are correct ${siteVisitor}!`);
+//   console.log(`You are correct ${siteVisitor}!`);
 } else if(question === 'n' || question === 'no'){
-  alert(`You are wrong ${siteVisitor}!`);
+//   alert(`You are wrong ${siteVisitor}!`);
   console.log(`You are wrong ${siteVisitor}!`);
 } else{
   alert(`Answer yes/no or y/n, ${siteVisitor}`);
-  console.log(`Answer yes/no or y/n, ${siteVisitor}`);
+//   console.log(`Answer yes/no or y/n, ${siteVisitor}`);
 }
