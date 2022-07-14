cdf5e41409ac39d90f0de4cb9dba266096a47eb4
Sue Young <m.sue.young@gmail.com>
2022-07-14 17:11:16 -0600

    added the score keeping

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Adding the top ten list, sixth and seventh questions. No functions!
 ### Links and Resources
 
 ### Reflections Lab 03
+It took longer than I thought.  I knew what I wanted to do.  And then I didn't build it slowly.  I went for the end game fast because I was worried I was behind.  Bad idea.  I usually develop very incrementally.  I am finding this is even more important in a scripting language, as the debugging tools are very limited. But still fun!  Proud of it! 
--- js/app.js
+++ js/app.js
@@ -1,95 +1,102 @@
 'use strict';
 
-// let siteVisitor = prompt('What is your name?');
-// alert(`Welcome to my site ${siteVisitor}! Guess these facts about me.`);
+let siteVisitor = prompt('What is your name?');
+alert(`Welcome to my site ${siteVisitor}! Guess these facts about me.`);
 
-// Sooooo much repeated code. NOOOOOOOOOOO! But I will bend to your wishes, Mistress.
+// Let the games begin!
+let score = 0;
 
-// let questionOne = prompt('Do I have children? (yes/no or y/n)').toLowerCase();
-// if(questionOne === 'y' || questionOne === 'yes'){
-//   alert('You are correct!');
-// //   console.log('You are correct!');
-// } else if(questionOne === 'n' || questionOne === 'no'){
-//   alert('You are wrong.');
-// //   console.log('You are wrong.');
-// } else{
-//   alert('Answer yes/no or y/n');
-// //   console.log('Answer yes/no or y/n');
-// }
+//Sooooo much repeated code. NOOOOOOOOOOO! But I will bend to your wishes, Mistress.
 
-// let questionTwo = prompt('Do I have one child? (yes/no or y/n)').toLowerCase();
-// if(questionTwo === 'y' || questionTwo === 'yes'){
-//   alert('You are correct!');
-// //   console.log('You are correct!');
-// } else if(questionTwo === 'n' || questionTwo === 'no'){
-//   alert('You are wrong.');
-// //   console.log('You are wrong.');
-// } else{
-//   alert('Answer yes/no or y/n');
-// //   console.log('Answer yes/no or y/n');
-// }
+let questionOne = prompt('Do I have children? (yes/no or y/n)').toLowerCase();
+if(questionOne === 'y' || questionOne === 'yes'){
+  alert('You are correct!');
+  score++;
+//   console.log('You are correct!');
+} else if(questionOne === 'n' || questionOne === 'no'){
+  alert('You are wrong.');
+//   console.log('You are wrong.');
+} else{
+  alert('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
+}
 
-// let question = prompt('Is it a boy? (yes/no or y/n)').toLowerCase();
-// if(question === 'y' || question === 'yes'){
-//   alert('You are correct!');
-// //   console.log('You are correct!');
-// } else if(question === 'n' || question === 'no'){
-//   alert('You are wrong.');
-// //   console.log('You are wrong.');
-// } else{
-//   alert('Answer yes/no or y/n');
-// //   console.log('Answer yes/no or y/n');
-// }
+let questionTwo = prompt('Do I have one child? (yes/no or y/n)').toLowerCase();
+if(questionTwo === 'y' || questionTwo === 'yes'){
+  alert('You are correct!');
+  score++;
+//   console.log('You are correct!');
+} else if(questionTwo === 'n' || questionTwo === 'no'){
+  alert('You are wrong.');
+//   console.log('You are wrong.');
+} else{
+  alert('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
+}
 
-// question = prompt('Is his favorite food asian? (yes/no or y/n)').toLowerCase();
-// if(question === 'y' || question === 'yes'){
-//   alert('You are correct!');
-// //   console.log('You are correct!');
-// } else if(question === 'n' || question === 'no'){
-//   alert('You are wrong.');
-// //   console.log('You are wrong.');
-// } else{
-//   alert('Answer yes/no or y/n');
-// //   console.log('Answer yes/no or y/n');
-// }
+let question = prompt('Is it a boy? (yes/no or y/n)').toLowerCase();
+if(question === 'y' || question === 'yes'){
+  alert('You are correct!');
+  score++;
+//   console.log('You are correct!');
+} else if(question === 'n' || question === 'no'){
+  alert('You are wrong.');
+//   console.log('You are wrong.');
+} else{
+  alert('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
+}
 
-// question = prompt('Does he like to play video games? (yes/no or y/n)').toLowerCase();
-// if(question === 'y' || question === 'yes'){
-//   alert(`You are correct ${siteVisitor}!`);
-// //   console.log(`You are correct ${siteVisitor}!`);
-// } else if(question === 'n' || question === 'no'){
-// //   alert(`You are wrong ${siteVisitor}!`);
-//   console.log(`You are wrong ${siteVisitor}!`);
-// } else{
-//   alert(`Answer yes/no or y/n, ${siteVisitor}`);
-// //   console.log(`Answer yes/no or y/n, ${siteVisitor}`);
-// }
+question = prompt('Is his favorite food asian? (yes/no or y/n)').toLowerCase();
+if(question === 'y' || question === 'yes'){
+  alert('You are correct!');
+  score++;
+//   console.log('You are correct!');
+} else if(question === 'n' || question === 'no'){
+  alert('You are wrong.');
+//   console.log('You are wrong.');
+} else{
+  alert('Answer yes/no or y/n');
+//   console.log('Answer yes/no or y/n');
+}
+
+question = prompt('Does he like to play video games? (yes/no or y/n)').toLowerCase();
+if(question === 'y' || question === 'yes'){
+  alert(`You are correct ${siteVisitor}!`);
+  score++;
+//   console.log(`You are correct ${siteVisitor}!`);
+} else if(question === 'n' || question === 'no'){
+//   alert(`You are wrong ${siteVisitor}!`);
+  console.log(`You are wrong ${siteVisitor}!`);
+} else{
+  alert(`Answer yes/no or y/n, ${siteVisitor}`);
+//   console.log(`Answer yes/no or y/n, ${siteVisitor}`);
+}
 
 // varibles used for both games
 let userGuess = 0;
 let counter = 0;
-let score = 0;
 let stillWrong = true;
 
 // guess a number game
-// let answer = Math.floor(Math.random() * 100);
+let answer = Math.floor(Math.random() * 100);
 
-// while (counter++ < 4 && stillWrong){
-//   userGuess = Number(prompt('Guess a number between 1-100.'));
-//   if (userGuess < answer) {
-//     alert('Your guess is too low.');
-//   } else if (userGuess > answer){
-//     alert('Your guess is too high.');
-//   } else {
-//     stillWrong = false;
-//     score = 1;
-//     alert('You are correct!');
-//   }
-// }
+while (counter++ < 4 && stillWrong){
+  userGuess = Number(prompt('Guess a number between 1-100.'));
+  if (userGuess < answer) {
+    alert('Your guess is too low.');
+  } else if (userGuess > answer){
+    alert('Your guess is too high.');
+  } else {
+    stillWrong = false;
+    score++;
+    alert('You are correct!');
+  }
+}
 
-// if (stillWrong){
-//   alert('The correct answer is: ' + answer);
-// }
+if (stillWrong){
+  alert('The correct answer is: ' + answer);
+}
 
 //guess one of my favorite foods game
 const foods = ['ramen', 'steak', 'salad', 'pizza', 'soup', 'pears', 'fries', 'pies'];
